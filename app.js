6c849d329fb2d7d74de72039d47f1d1528676100
kevinbloomquist <rkevinbloom@gmail.com>
2016-11-29 18:38:23 -0700

    create works but I need to add return response

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var url = "mongodb://localhost:27017/restaurant_db";
 
 mongo.connect(url,function(err,db){
 	var collection = db.collection('restaurants');
-	/*allChoice*************************************************
+	
 	var allChoice = prompt("Type 'all' and press enter to display all restaurants'names:");
 	if (allChoice == "all"){
 		collection.find().toArray(function(err,doc){
@@ -13,19 +13,20 @@ mongo.connect(url,function(err,db){
 	}else {
 		console.log("MMMM, restaurants see you don't want to?");
 	}
-}); (remade this cluster with each exercise/ only need 1 if all are uncommented)
-	end allChoice***************************************************/
-	/*begin oneChoice***********************************************
+
+	
+	// choose one restaurant
 	var oneChoice = prompt("Type a restaurant name and press enter to view:");
 	collection.find().forEach(function(item,index){
 	if (oneChoice == item.name){
 			console.log(item);
 	}
 	});
-});(remade this cluster with each exercise)
-	end oneChoice*****************************************************/
+	// create restaurant
 	var mkRestName = prompt("Type a restaurant name to begin creating a restaurant:");
-	// var mkRestAddress = prompt("Type the restaurant's address:");
-	// var newRest = collection.insert({"name":mkRestName,"address":mkRestAddress});
 	console.log(mkRestName);
+	var mkRestAddress = prompt("Type the restaurant's address:");
+	console.log(mkRestAddress);
+	collection.insert({"name":mkRestName,"address":mkRestAddress});
+	console.log();
 });
