38228ee0fd7f2162b447b1c9e0eabc2daf67fae0
kevinbloomquist <rkevinbloom@gmail.com>
2016-11-29 17:59:46 -0700

    safety commit

--- app.js
+++ app.js
@@ -4,13 +4,28 @@ var url = "mongodb://localhost:27017/restaurant_db";
 
 mongo.connect(url,function(err,db){
 	var collection = db.collection('restaurants');
+	/*allChoice*************************************************
 	var allChoice = prompt("Type 'all' and press enter to display all restaurants'names:");
 	if (allChoice == "all"){
 		collection.find().toArray(function(err,doc){
 			console.log(doc);
 		});
 	}else {
-		console.log("MMMM, Restaurants see you don't want to?");
+		console.log("MMMM, restaurants see you don't want to?");
+	}
+}); (remade this cluster with each exercise/ only need 1 if all are uncommented)
+	end allChoice***************************************************/
+	/*begin oneChoice***********************************************
+	var oneChoice = prompt("Type a restaurant name and press enter to view:");
+	collection.find().forEach(function(item,index){
+	if (oneChoice == item.name){
+			console.log(item);
 	}
 	});
-	
+});(remade this cluster with each exercise)
+	end oneChoice*****************************************************/
+	var mkRestName = prompt("Type a restaurant name to begin creating a restaurant:");
+	// var mkRestAddress = prompt("Type the restaurant's address:");
+	// var newRest = collection.insert({"name":mkRestName,"address":mkRestAddress});
+	console.log(mkRestName);
+});
